e744b3ca2df48aa268d8b15c09b290fa1616d985
Stray2222 <nnu2150@gmail.com>
2023-03-04 17:48:46 +0500

    Bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beancat =
+                (Cat) applicationContext.getBean("cat");
+        Cat beancat1 =
+                (Cat) applicationContext.getBean("cat");
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+        System.out.println(bean.equals(bean2));
+
+        System.out.println(beancat.getMessage());
+        System.out.println(beancat1.getMessage());
+        System.out.println(beancat.equals(beancat1));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,10 @@
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMassege (String message) {
+        this.message = message;
+    }
+}
